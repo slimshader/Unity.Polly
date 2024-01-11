712c8aa7f182343abb4f1291ae6858460f523206
Szymon Gatner <szymon.gatner@vox.pl>
2024-01-11 15:19:01 +0100

    polly scr 7.4.2

---
+++ Packages/Unity.Polly/Runtime/Utilities/KeyHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Polly.Utilities
+{
+    internal static class KeyHelper
+    {
+        public static string GuidPart() => Guid.NewGuid().ToString().Substring(0, 8);
+    }
+}
